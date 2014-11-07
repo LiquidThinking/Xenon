97f758ae013f2800172edf889f65f655eb82bf4d
Adeel <adeel41@yahoo.com>
2014-11-07 16:04:47 +0000

    resolved #15 by implementing SwitchToWindow and CloseCurrentAndSwitchToWindow methods

---
+++ Xenon.Tests/SwitchToWindowTests/XenonScreenSwitchToWindowTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Xenon.Tests.SwitchToWindowTests
+{
+	[TestFixture]
+	public class XenonScreenSwitchToWindowTests : BaseSwitchToWindowTests<DummyXenonScreen>
+	{
+		protected override BaseXenonTest<DummyXenonScreen> CreateInstance( IXenonBrowser browser )
+		{
+			return new DummyXenonScreen( browser );
+		}
+	}
+}
